6bfa73ece3bc949bd152890a9e5906b1d9729422
Ido <Ido>
2024-07-04 23:39:16 +0300

    change layout to homepage

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,25 +4,27 @@ import React from 'react';
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import {HomeScreenPage} from "@/app/pages/HomeScreen";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+      <HomeScreenPage/>
+    // <Tabs
+    //   screenOptions={{
+    //     tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+    //     headerShown: false,
+    //   }}>
+    //   <Tabs.Screen
+    //     name="index"
+    //     options={{
+    //       title: 'Home',
+    //       tabBarIcon: ({ color, focused }) => (
+    //         <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+    //       ),
+    //     }}
+    //   />
+    // </Tabs>
   );
 }
