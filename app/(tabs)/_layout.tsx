f3634ed461d390dcfd91c3da457f05ad3c999840
Ido <Ido>
2024-07-04 23:43:04 +0300

    add todos

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,16 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from "expo-router";
+import React from "react";
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-import {HomeScreenPage} from "@/app/pages/HomeScreen";
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { HomeScreenPage } from "@/app/pages/HomeScreen";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
-
+  //todo: delete comment
   return (
-      <HomeScreenPage/>
+    <HomeScreenPage />
     // <Tabs
     //   screenOptions={{
     //     tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
