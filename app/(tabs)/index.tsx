8951ad21346a2e9165603aac082686387cebfe99
Ido <Ido>
2024-07-04 23:44:50 +0300

    change app to HomeScreenPage

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,16 @@
-import App from "@/App";
-import {OneSignal} from "react-native-onesignal";
+import { OneSignal } from "react-native-onesignal";
 import Constants from "expo-constants";
+import { HomeScreenPage } from "../pages/HomeScreen";
 
 function oneSignal() {
-    OneSignal.initialize(Constants.expoConfig!.extra!.oneSignalAppId);
+  OneSignal.initialize(Constants.expoConfig!.extra!.oneSignalAppId);
 
-// Also need enable notifications to complete OneSignal setup
-    OneSignal.Notifications.requestPermission(true);
+  // Also need enable notifications to complete OneSignal setup
+  OneSignal.Notifications.requestPermission(true);
 }
 
 export default function HomeScreen() {
-    oneSignal()
+  oneSignal();
 
-    return (
-        <App/>
-    );
+  return <HomeScreenPage />;
 }
