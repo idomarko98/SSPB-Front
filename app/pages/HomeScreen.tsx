c5cd81b9376d87fbdfdd6ab93280f37bf79ee51a
Ido <Ido>
2024-07-07 15:46:30 +0300

    add home screen page

---
+++ app/pages/HomeScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Navigatiotr} from "@/components/test components/Navigator";
+import MapComponent from "@/components/map/MapComponent";
+
+export function HomeScreenPage() {
+
+    return (
+        <MapComponent />
+    );
+}
