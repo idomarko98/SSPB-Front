62466a2a75b0282177cfdf964e6e3037430e40bf
Ido <Ido>
2024-07-07 16:12:00 +0300

    remove redundant import

--- app/pages/HomeScreen.tsx
+++ app/pages/HomeScreen.tsx
@@ -1,10 +1,6 @@
 import React from "react";
-import {Navigatiotr} from "@/components/test components/Navigator";
 import MapComponent from "@/components/map/MapComponent";
 
 export function HomeScreenPage() {
-
-    return (
-        <MapComponent />
-    );
+  return <MapComponent />;
 }
