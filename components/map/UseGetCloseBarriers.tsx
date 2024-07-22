fd749be307dfa86de5ceadf49ae8595e8a2f2142
Ido <Ido>
2024-07-22 22:15:28 +0300

    use setBarriers inside hook in order to reflect change

--- components/map/UseGetCloseBarriers.tsx
+++ components/map/UseGetCloseBarriers.tsx
@@ -1,6 +1,6 @@
 import { getDatabase, ref } from "@firebase/database";
 import { GeoFire } from "geofire";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const BARRIERS_LOCATION_REF = "BarriersLocations";
 
@@ -11,49 +11,66 @@ export function useGetCloseBarriers(
   const [barriers, setBarriers] = useState<BarrierBasicInfo[]>([]);
 
   function addBarrier(barrierId: string, location: [number, number]) {
-    barriers.push({
+    const newBarrier = {
       id: barrierId,
       location: { latitude: location[0], longitude: location[1] },
-    });
+    };
+
+    const updatedBarriers = [...barriers, newBarrier];
+    setBarriers(updatedBarriers);
   }
 
   function removeBarrier(barrierId: string, location: [number, number]) {
-    const index = barriers.indexOf({
+    const indexToRemove = barriers.indexOf({
       id: barrierId,
       location: { latitude: location[0], longitude: location[1] },
     });
-    if (index > -1) {
-      barriers.splice(index, 1);
-    }
+
+    setBarriers((barriers) =>
+      barriers.filter((barrier, index) => index !== indexToRemove)
+    );
   }
 
-  const firebaseRef = ref(getDatabase(), BARRIERS_LOCATION_REF);
-  const geoFireInstance: GeoFire = new GeoFire(firebaseRef);
+  useEffect(() => {
+    const firebaseRef = ref(getDatabase(), BARRIERS_LOCATION_REF);
+    const geoFireInstance: GeoFire = new GeoFire(firebaseRef);
 
-  const geoQuery = geoFireInstance.query({
-    center: [currentLocation.latitude, currentLocation.longitude],
-    radius: radiusInKm,
-  });
+    const geoQuery = geoFireInstance.query({
+      center: [currentLocation.latitude, currentLocation.longitude],
+      radius: radiusInKm,
+    });
 
-  geoQuery.on(
-    "key_entered",
-    function (key: string, location: [number, number]) {
-      addBarrier(key, location);
-    }
-  );
+    geoQuery.on(
+      "key_entered",
+      function (key: string, location: [number, number]) {
+        console.log("Entered");
 
-  geoQuery.on("key_exited", function (key: string, location: [number, number]) {
-    removeBarrier(key, location);
-  });
+        addBarrier(key, location);
+      }
+    );
 
-  geoQuery.on("key_moved", function (key: string, location: [number, number]) {
-    removeBarrier(key, location);
-    addBarrier(key, location);
-  });
+    geoQuery.on(
+      "key_exited",
+      function (key: string, location: [number, number]) {
+        removeBarrier(key, location);
+      }
+    );
 
-  geoQuery.on("ready", function (key: string, location: [number, number]) {
-    // console.log("wow");
-  });
+    geoQuery.on(
+      "key_moved",
+      function (key: string, location: [number, number]) {
+        removeBarrier(key, location);
+        addBarrier(key, location);
+      }
+    );
 
+    geoQuery.on("ready", function (key: string, location: [number, number]) {
+      console.log("wow");
+      console.log(currentLocation, "this is the current location");
+      console.log(radiusInKm, "this is the current radius");
+    });
+  }, [currentLocation, radiusInKm]);
+
+  // geoFireInstance.set("barrier1", [32.3354366, 34.8616168]);
   return [barriers, setBarriers];
 }
